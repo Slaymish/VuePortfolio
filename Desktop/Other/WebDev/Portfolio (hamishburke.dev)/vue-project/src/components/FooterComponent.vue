a7be66619a11f3f9cd7b0a9db75268c621e99df0
Hamish <hamishapps@gmail.com>
2023-04-25 19:02:57 +1200

    Add existing file

---
+++ Desktop/Other/WebDev/Portfolio (hamishburke.dev)/vue-project/src/components/FooterComponent.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export default {
+        name: 'FooterComponent',
+        props: {
+            email: String,
+            githubName: String,
+            instagramName: String,
+            linkedinName: String,
+        }
+    }
+</script>
+
+<template>
+    <div class="footer-wrapper">
+        <div class="footer">
+            <div class="footer__socials">
+                <a :href="'mailto:' + email" target="_blank" rel="noopener noreferrer">
+                    <v-icon>mdi-email</v-icon>
+                </a>
+                <a :href="'https://www.github.com/' + githubName" target="_blank" rel="noopener noreferrer">
+                    <v-icon>mdi-github</v-icon>
+                </a>
+                <a :href="'https://www.instagram.com/' + instagramName" target="_blank" rel="noopener noreferrer">
+                    <v-icon>mdi-instagram</v-icon>
+                </a>
+                <a :href="'https://www.linkedin.com/in/' + linkedinName" target="_blank" rel="noopener noreferrer">
+                    <v-icon>mdi-linkedin</v-icon>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
